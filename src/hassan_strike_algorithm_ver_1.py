69a1c8d4e0340525ad63ff9c9456eabec460a063
ChromaChroma <jona.beer@gmail.com>
2023-10-06 13:50:10 +0200

    Add Hassan's offline algorithm

---
+++ src/hassan_strike_algorithm_ver_1.py
@@ -0,0 +1,45 @@
+from queue import PriorityQueue
+
+
+def strike_algorithm(n, m, days, extra_info=False, debug_info=False):
+    ordered_days = PriorityQueue(m)
+
+    cumulative_cost = 0
+    count = 0
+
+    if debug_info:
+        print('------------- Start{Generating Day Values} -------------')
+    for (seat, flight_cost, hotel_cost) in days:
+        price = flight_cost + cumulative_cost
+
+        if debug_info:
+            print(f'day: {count}, price:{price}')
+
+        ordered_days.put((price, (count, seat, flight_cost, hotel_cost)))
+
+        cumulative_cost = cumulative_cost + hotel_cost
+        count = count + 1
+    if debug_info:
+        print('------------- End{Generating Day Values  } -------------\n')
+
+    travel_log = list(range(0, m))
+    remaining = n
+    while not ordered_days.empty():
+        (price, (day, seat, flight_cost, hotel_cost)) = ordered_days.get();
+        travelers = min(seat, remaining)
+        travel_log[day] = travelers
+        remaining = remaining - travelers
+
+    remaining = n
+    cost_tracker = 0
+    for i in range(0, m):
+        travelers = travel_log[i]
+        (seat, flight_cost, hotel_cost) = days[i]
+        remaining = remaining - travelers
+        cost_tracker = cost_tracker + (travelers * flight_cost) + (hotel_cost * remaining)
+        if (extra_info):
+            print(f'travelers: {travelers}, loungers: {remaining};\n'
+                  f'travel cost: {travelers * flight_cost}, hotel cost: {hotel_cost * remaining};\n'
+                  f'cumulative cost this far: {cost_tracker}\n')
+        else:
+            print(f'{travelers}, {remaining}')
