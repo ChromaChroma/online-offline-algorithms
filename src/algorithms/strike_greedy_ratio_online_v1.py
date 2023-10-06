932b444ca6c576d0ac692c6196aee80481cc3cd1
ChromaChroma <jona.beer@gmail.com>
2023-10-06 15:33:59 +0200

    Add todo version of greedy ratio

---
+++ src/algorithms/strike_greedy_ratio_online_v1.py
@@ -0,0 +1,42 @@
+"""
+Improvements to be added:
+
+* If hotel price > flight, send maximum amount
+* If hotel price is, within ratio, slightly lower than flight, send maximum/partial
+
+* Later, alter ratio based on historical data and averages, and compare those with current prices. (with ratio)
+
+"""
+def strike_algorithm(n: int, m: int, days: (int, int, int), ratio: float = 0) -> (int, [(int, int)]):
+    """
+    Greedy online algorithm with ratio
+
+    :param n: Number of people to be sent back
+    :param m: Days within all n people should be sent back
+    :param days: m long list of tuple with information per day including
+                (amount_of_seats, flight_cost, hotel_cost)
+    :returns: tuple (total_cost, list of people sent and remaining each day)
+    """
+    total_cost = 0
+    cumulative_cost = 0
+    day_number = 0
+
+    sent_and_remaining = []
+
+    for (seats, flight_cost, hotel_cost) in days:
+        if n == 0:
+            # Fill remaining days with (0,0)
+            sent_and_remaining += [(0, 0) for _ in range(1, m - day_number)]
+            break
+        people_sent_back = seats if seats <= n else n
+
+        price = flight_cost + cumulative_cost
+        total_cost += price * people_sent_back
+        n -= people_sent_back
+
+        sent_and_remaining.append((people_sent_back, n))
+
+        cumulative_cost += hotel_cost
+        day_number += 1
+
+    return total_cost, sent_and_remaining
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import algorithms.hassan_strike_algorithm_ver_1 as offline
 import algorithms.strike_greedy_online_v1 as greedy
+import algorithms.strike_greedy_ratio_online_v1 as greedy_ratio
 from instance_parser import load_instance
 
 # Instance files:
@@ -10,8 +11,11 @@ I_cheaper_later = "high-first-high-first-cheaper-later.txt"
 if __name__ == '__main__':
     random_instance = load_instance(I_random)
 
-    v = greedy.strike_algorithm(*random_instance)
-    print(v)
+    greedy_ratio_results = greedy_ratio.strike_algorithm(*random_instance)
+    print(greedy_ratio_results)
+
+    greedy_results = greedy.strike_algorithm(*random_instance)
+    print(greedy_results)
     greedy.strike_algorithm_print(*random_instance, debug_info=True, extra_info=True)
 
     offline.strike_algorithm(*random_instance, debug_info=True, extra_info=False)
