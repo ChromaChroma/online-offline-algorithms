9533c9e9ba964c1696e29fafbec84ada18e5ebf7
ChromaChroma <jona.beer@gmail.com>
2023-10-11 02:04:34 +0200

    Add online ratio algorithm. Add plots to main Add loop to check total cost for 0 to 1

--- src/algorithms/strike_greedy_ratio_online_v1.py
+++ src/algorithms/strike_greedy_ratio_online_v1.py
@@ -7,7 +7,9 @@ Improvements to be added:
 * Later, alter ratio based on historical data and averages, and compare those with current prices. (with ratio)
 
 """
-def strike_algorithm(n: int, m: int, days: (int, int, int), ratio: float = 0) -> (int, [(int, int)]):
+
+
+def strike_algorithm(n: int, m: int, days: (int, int, int), ratio: float = 1) -> (int, [(int, int)]):
     """
     Greedy online algorithm with ratio
 
@@ -15,6 +17,8 @@ def strike_algorithm(n: int, m: int, days: (int, int, int), ratio: float = 0) ->
     :param m: Days within all n people should be sent back
     :param days: m long list of tuple with information per day including
                 (amount_of_seats, flight_cost, hotel_cost)
+    :param ratio: Ratio of flight price to choose flight over hotel stay, 0≤ratio≤1
+
     :returns: tuple (total_cost, list of people sent and remaining each day)
     """
     total_cost = 0
@@ -28,13 +32,17 @@ def strike_algorithm(n: int, m: int, days: (int, int, int), ratio: float = 0) ->
             # Fill remaining days with (0,0)
             sent_and_remaining += [(0, 0) for _ in range(1, m - day_number)]
             break
-        people_sent_back = seats if seats <= n else n
 
-        price = flight_cost + cumulative_cost
-        total_cost += price * people_sent_back
-        n -= people_sent_back
+        if (flight_cost <= hotel_cost) or (flight_cost * ratio <= hotel_cost):
+            people_sent_back = seats if seats <= n else n
 
-        sent_and_remaining.append((people_sent_back, n))
+            price = flight_cost + cumulative_cost
+            total_cost += price * people_sent_back
+            n -= people_sent_back
+
+            sent_and_remaining.append((people_sent_back, n))
+        else:
+            sent_and_remaining.append((0, n))
 
         cumulative_cost += hotel_cost
         day_number += 1
--- src/main.py
+++ src/main.py
@@ -2,20 +2,48 @@ import algorithms.hassan_strike_algorithm_ver_1 as offline
 import algorithms.strike_greedy_online_v1 as greedy
 import algorithms.strike_greedy_ratio_online_v1 as greedy_ratio
 from instance_parser import load_instance
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Instance files:
 I_random = "random.txt"
 I_high_first = "high-first-not-enough-later.txt"
 I_cheaper_later = "high-first-high-first-cheaper-later.txt"
+I_const_hotel = "constant-hotel-price.txt"
 
 if __name__ == '__main__':
-    random_instance = load_instance(I_random)
+    instance = load_instance(I_const_hotel)
+    (opt_total, p) = (999999, [])
+    opt = 0
+    points = []
 
-    greedy_ratio_results = greedy_ratio.strike_algorithm(*random_instance)
-    print(greedy_ratio_results)
+    (t_offline, p_offline) = offline.strike_algorithm(*instance, debug_info=True, extra_info=True)
+    offline_optimal = 3040
+    plt.axhline(y=t_offline, color='r', linestyle='-', label='Offline')
 
-    greedy_results = greedy.strike_algorithm(*random_instance)
-    print(greedy_results)
-    greedy.strike_algorithm_print(*random_instance, debug_info=True, extra_info=True)
+    # Greedy algorithm
+    (total, people) = greedy.strike_algorithm(*instance)
+    print(f"Greedy with total={total},\n for=  {people}")
+    plt.axhline(y=total, color='g', linestyle='-', label='Online')
+    # greedy.strike_algorithm_print(*random_instance, debug_info=True, extra_info=True)
 
-    offline.strike_algorithm(*random_instance, debug_info=True, extra_info=False)
+    for ratio in [i * 0.01 for i in range(1, 100)]:
+        (total, people) = greedy_ratio.strike_algorithm(*instance, ratio=ratio)
+
+        if total < opt_total and people[-1][1] == 0:
+            points.append(total)
+            opt_total = total
+            p = people
+            opt = ratio
+
+    # Print and plot greedy ratio
+    print(f"opt ratio={opt}, with total={opt_total},\n for=  {p}")
+    plt.plot(np.array(points), color='b', linestyle='dotted')
+    plt.axhline(y=opt_total, color='b', linestyle='-', label='Online Ratio')
+    # greedy_ratio_results = greedy_ratio.strike_algorithm(*instance, ratio=1)
+    # print(greedy_ratio_results)
+
+    plt.legend()
+    plt.title("Total price per algorithm (lower = better)")
+    plt.ylabel("Total cost")
+    plt.show()
